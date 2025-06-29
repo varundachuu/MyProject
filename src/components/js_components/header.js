db1e080029575ab4c7a41d6487d4bc49f7bac229
varundachuu <varundachuu2000@gmail.com>
2025-06-29 06:24:39 -0700

    Update header.js

--- src/components/js_components/header.js
+++ src/components/js_components/header.js
@@ -31,7 +31,7 @@ function Header() {
     <header className="header">
       {/* Logo Section */}
       <div className="logo">
-        <img src="./images/logo2.png" alt="logo" />
+        <img src="./images/log4.jpg" alt="logo" />
       </div>
 
       {/* Navigation Links */}
