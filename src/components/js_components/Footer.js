529f02c77bca58a55bd18bbcec200a93ecfe1803
varundachuu <varundachuu2000@gmail.com>
2025-07-13 11:16:57 -0700

    Update Footer.js

--- src/components/js_components/Footer.js
+++ src/components/js_components/Footer.js
@@ -43,7 +43,7 @@ const Footer = () => {
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; 2024 OneQuickSolutions. All Rights Reserved.</p>
+        <p>&copy; 2025 OneQuickSolutions. All Rights Reserved.</p>
       </div>
     </footer>
   );
