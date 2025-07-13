991e84160cd10cc7c2dcd28a649c59a251c25078
varundachuu <varundachuu2000@gmail.com>
2025-07-13 14:15:13 +0530

    Update ScrollToTop.js

--- src/components/js_components/ScrollToTop.js
+++ src/components/js_components/ScrollToTop.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "../css-files/other ScrollToTop.css";
 
 const ScrollToTop = () => {
   const [isVisible, setIsVisible] = useState(false);
