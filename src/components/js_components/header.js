d58fc4c894c4492ca660ad13bce578521adf7c21
varundachuu <varundachuu2000@gmail.com>
2025-09-22 23:02:30 +0530

    fixing header file

--- src/components/js_components/header.js
+++ src/components/js_components/header.js
@@ -66,6 +66,7 @@ function Header() {
         <Link onClick={() => { scrollToSection("features"); setIsMenuOpen(false);}}>Features</Link>
         <Link onClick={() => { scrollToSection("service"); setIsMenuOpen(false);}}>Services</Link>
         <Link onClick={() => { scrollToSection("OtherServices"); setIsMenuOpen(false);}}>Programmes</Link>   
+        <Link onClick={() => { scrollToSection("OtherServices"); setIsMenuOpen(false);}}>Products</Link> 
         <Link onClick={() => { scrollToSection("contact"); setIsMenuOpen(false);}}>Contact Us</Link>
            </div>
       </div>
@@ -74,5 +75,6 @@ function Header() {
 }
 
 export default Header;
+
 
 
