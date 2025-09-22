44aacbdc8e136dc08d38bd1f4e16d91fcd27df9f
varundachuu <varundachuu2000@gmail.com>
2025-09-22 22:27:19 +0530

    Update header.js

--- src/components/js_components/header.js
+++ src/components/js_components/header.js
@@ -42,6 +42,7 @@ function Header() {
         <Link onClick={() => { scrollToSection("features"); setIsMenuOpen(false);}}>Features</Link>
         <Link onClick={() => { scrollToSection("service"); setIsMenuOpen(false);}}>Services</Link>
         <Link onClick={() => { scrollToSection("OtherServices"); setIsMenuOpen(false);}}>Programmes</Link>   
+        <Link onClick={() => { scrollToSection("OtherServices"); setIsMenuOpen(false);}}>Products</Link>   
         <Link onClick={() => { scrollToSection("contact"); setIsMenuOpen(false);}}>Contact Us</Link>
         
   </nav>
@@ -73,3 +74,4 @@ function Header() {
 }
 
 export default Header;
+
